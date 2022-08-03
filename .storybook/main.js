ac71ebc597b1e9b8169b8d7db5075c12695ce3c2
Subin Paul <subin01@gmail.com>
2022-08-03 20:09:23 +1000

    feat: storybook goodies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,27 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@whitespace/storybook-addon-html",
+    "@storybook/addon-a11y",
+    "storybook-addon-designs",
+    "@hover/storybook-addon-pseudo-states",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+};
